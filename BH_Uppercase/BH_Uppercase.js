84207b58646eb9a3ab52fe0f3dad749c9c65bca1
Brad Harrison <bharrison@buckwold.com>
2025-09-19 08:14:53 -0600

    CYA

--- BH_Uppercase/BH_Uppercase.js
+++ BH_Uppercase/BH_Uppercase.js
@@ -1,4 +1,15 @@
+/* =============================================================================
+   DISCLAIMER
+   =============================================================================
+   This script is provided "AS IS", without warranty of any kind, express or
+   implied, including but not limited to the warranties of merchantability,
+   fitness for a particular purpose, and noninfringement.
 
+   Use at your own risk. In no event shall the authors or copyright holders be
+   liable for any claim, damages, or other liability, whether in an action of
+   contract, tort, or otherwise, arising from, out of, or in connection with
+   the script or the use or other dealings in the script.
+   ============================================================================= */
 
 (function(){
     "use strict";
